0b2df6ed645721781f852eb0a0ae84374ed0328e
João Santos <joaopns05@gmail.com>
2021-03-10 16:19:47 +0000

    improve docker scripts

--- scripts/docker-blog.sh
+++ scripts/docker-blog.sh
@@ -1,7 +1,4 @@
 set -x
 docker rm -f blog
-docker network rm frontend
-docker network create frontend
-docker network connect frontend backend
 docker build -f Dockerfile -t blog .
-docker run --name blog --network frontend -p 3000:80 -d blog
+docker run --name blog --network blog -p 3000:80 -d blog
